816c4fe5a9667e4227de0d1c72d9507206e1484a
Moises981 <jesus.castillo.j@tecsup.edu.pe>
2022-08-26 22:44:05 -0500

    Lifecycle Logging

--- app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("Lifecycle", "OnCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle", "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle", "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle", "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle", "OnStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle", "OnRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "OnDestroy")
+    }
+
 }
